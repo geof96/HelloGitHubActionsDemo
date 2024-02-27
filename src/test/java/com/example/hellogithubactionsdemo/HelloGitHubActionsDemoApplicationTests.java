4863ab72ea2d2c9cc4b3d5f11cda2331a1a42a58
geof96 <leyton2730@gmail.com>
2024-02-27 10:49:21 +0100

    Initial commit

---
+++ src/test/java/com/example/hellogithubactionsdemo/HelloGitHubActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellogithubactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGitHubActionsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
